6a5be89751235042002ba875aee9ac0aab6a533f
Gleb Kozhukhovskii <glebsanych@gmail.com>
2025-07-10 11:12:25 +0300

    Выполнены задачи 1.1 - 1.5

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,5 +1,5 @@
 function checkSpam(str) {
-  // ваш код... '1xBet' или 'XXX'
+  // ваш код.. '1xBet' или 'XXX'
   const lstr = str.toLowerCase()
   return lstr.includes('1xBet'.toLowerCase() ) || lstr.includes('XXX'.toLowerCase() );
 }
