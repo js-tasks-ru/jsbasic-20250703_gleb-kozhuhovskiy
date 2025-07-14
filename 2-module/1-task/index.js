295b1bb30c9f05443c5e77b4b6fd5ea828a4f6b8
Gleb Kozhukhovskii <glebsanych@gmail.com>
2025-07-14 07:59:08 +0300

    Done all ex module 2

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,32 @@
 function sumSalary(salaries) {
   // ваш код...
+  if( !salaries || !(typeof salaries === 'object') ){
+    return 0;
+  }
+
+  let totSal=0;
+  for(let p in salaries){
+    const v = salaries[p];
+     if( (typeof v === 'number') && v && isFinite( v ) ){
+        totSal += v; 
+     }
+  }
+  return totSal;
 }
+
+
+// let salaries = {
+//   John1: NaN,
+//   John: 1000,
+//   Ann: 1600,
+//   Pete: 1300,
+//   month: 'December',
+//   currency: 'USD',
+//   isPayed: false
+// }
+
+// console.log( isFinite( 1000 ))
+// console.log( sumSalary(1) )
+
+// console.log( sumSalary(salaries) )
+
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,15 @@
 function isEmpty(obj) {
   // ваш код...
+  for(let p in obj){
+    return false;
+  }
+  return true;
 }
+
+// let schedule = {};
+
+// console.log( isEmpty(schedule) ); // true
+
+// schedule["8:30"] = "подъём";
+
+// console.log( isEmpty(schedule) ); // false
