1858438af4c7d612d400192c152dc8a0f85eced2
Gleb Kozhukhovskii <glebsanych@gmail.com>
2025-07-29 13:38:55 +0300

    All ex module 5 done!

--- 5-module/1-task/index.js
+++ 5-module/1-task/index.js
@@ -1,3 +1,8 @@
 function hideSelf() {
   // ваш код...
+  const elem = document.querySelector(".hide-self-button")
+  
+  elem.addEventListener('click', (event) => {
+    elem.hidden = true;
+  });
 }
--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,10 @@
 function toggleText() {
   // ваш код...
+  const elemBtn = document.querySelector(".toggle-text-button")
+  
+  elemBtn.addEventListener('click', (event) => {
+    const elemTxt = document.querySelector("#text");
+    elemTxt.hidden = !elemTxt.hidden;
+  });
+
 }
